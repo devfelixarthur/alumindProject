36c747ed9b8462d1a902f6b033b701dde14e0867
arthur_andrade_2024 <arthur.andrade@omnilink.com.br>
2025-03-27 22:42:17 -0300

    [Backend] - Estrutura inicial do projeto

---
+++ Backend/alumind/src/test/java/com/api/v1/alumind/AlumindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.v1.alumind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
