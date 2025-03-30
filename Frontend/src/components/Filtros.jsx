dfef530b90623941b1a97be2c83384d82a0bb663
arthur_andrade_2024 <arthur.andrade@omnilink.com.br>
2025-03-30 01:37:16 -0300

    Atualizando dos filtros

--- Frontend/src/components/Filtros.jsx
+++ Frontend/src/components/Filtros.jsx
@@ -6,36 +6,41 @@ import {
   Input,
   VStack,
   Icon,
+  Portal, 
+  Select, 
+  createListCollection 
 } from '@chakra-ui/react';
 import { FiSearch } from 'react-icons/fi';
 import { useState, useEffect } from 'react';
 
+const options = createListCollection ({
+  items: [
+    { label: "Positivo", value: "POSITIVO" },
+    { label: "Negativo", value: "NEGATIVO" },
+    { label: "Neutro", value: "NEUTRO" },
+    { label: "Inconclusivo", value: "INCONCLUSIVO" },
+  ],
+})
+
 const Filtros = ({ onFilter, defaultStartDate, defaultEndDate }) => {
   const [startDate, setStartDate] = useState(defaultStartDate);
   const [endDate, setEndDate] = useState(defaultEndDate);
-  const [code, setCode] = useState('');
   const [id, setId] = useState('');
+  const [sentiment] = useState('');
 
   useEffect(() => {
-    // Atualiza as datas quando o componente receber novas props
     setStartDate(defaultStartDate);
     setEndDate(defaultEndDate);
   }, [defaultStartDate, defaultEndDate]);
 
   const handleApplyFilters = () => {
-    onFilter({ startDate, endDate, code, id });
+    onFilter({ startDate, endDate, sentiment, id });
   };
 
   return (
     <Box bg="white" p={4} borderRadius="md" shadow="md" w="full">
       <VStack align="stretch" spacing={4}>
         <HStack spacing={4} align="stretch">
-          <Input
-            placeholder="Código"
-            flex={1}
-            value={code}
-            onChange={(e) => setCode(e.target.value)}
-          />
           <Input
             type="date"
             flex={1}
@@ -54,6 +59,34 @@ const Filtros = ({ onFilter, defaultStartDate, defaultEndDate }) => {
             value={id}
             onChange={(e) => setId(e.target.value)}
           />
+          <Select.Root
+              collection={options}
+              defaultValue={["spirited_away"]}
+              width="300px"
+            >
+              <Select.HiddenSelect/>
+              <Select.Control>
+                <Select.Trigger>
+                  <Select.ValueText placeholder="Selecione uma Opção" />
+                </Select.Trigger>
+                <Select.IndicatorGroup>
+                  <Select.ClearTrigger />
+                  <Select.Indicator />
+                </Select.IndicatorGroup>
+              </Select.Control>
+              <Portal>
+                <Select.Positioner>
+                  <Select.Content>
+                    {options.items.map((option) => (
+                      <Select.Item item={option} key={option.value}>
+                        {option.label}
+                        <Select.ItemIndicator />
+                      </Select.Item>
+                    ))}
+                  </Select.Content>
+                </Select.Positioner>
+              </Portal>
+            </Select.Root>
         </HStack>
         <Flex gap={4}>
           <Button leftIcon={<Icon as={FiSearch} />} onClick={handleApplyFilters} colorScheme="blackAlpha">
